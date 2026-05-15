4af49635b49836e82852ffab0b7d8d47f453ba71
bbadgujar961-design <bbadgujar961@gmail.com>
2026-05-16 00:03:06 +0530

    Delete pharmacy_management_db.sql

--- pharmacy_management_db.sql
+++
@@ -1,8 +0,0 @@
-USE pharmacy_management_db;
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    phone_number VARCHAR(15) UNIQUE NOT NULL,
-    role VARCHAR(50) DEFAULT 'Pharmacist'
-);
-
-INSERT INTO users (phone_number, role) VALUES ('7020712960', 'Admin');
